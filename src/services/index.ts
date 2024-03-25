22275e446d2973fd8462a2cf57524d3abe51bcac
JuneJH <15310487388@163.com>
2024-03-25 22:39:50 +0800

    feat(*): 添加拦截器

--- src/services/index.ts
+++ src/services/index.ts
@@ -6,5 +6,27 @@ const request = axios.create({
 })
 
 
+// 添加请求拦截器
+request.interceptors.request.use(
+    function (config) {
+        // 在发送请求之前做些什么，比如添加认证信息、设置请求头等
+        config.headers.Authorization = 'Bearer your_access_token'; // 替换为你的认证信息
+        return config;
+    },
+    function (error) {
+        // 对请求错误做些什么
+        return Promise.reject(error);
+    }
+);
+request.interceptors.response.use(
+    function (response) {
+        // 对响应数据做些什么，比如处理特定的错误状态码
+        return response;
+    },
+    function (error) {
+        // 对响应错误做些什么，比如处理网络错误或者服务端返回的错误信息
+        return Promise.reject(error);
+    }
+);
 
 export default request;
