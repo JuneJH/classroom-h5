fd504db4905f2f1d9e3fcc8daee43cffb622606b
JuneJH <15310487388@163.com>
2024-05-01 21:20:50 +0800

    feat(*): 移动端

--- src/.umi/core/route.tsx
+++ src/.umi/core/route.tsx
@@ -19,12 +19,13 @@ if (process.env.NODE_ENV === 'development') {
 import React from 'react';
 
 export async function getRoutes() {
-  const routes = {"Classfiy/index":{"path":"Classfiy","id":"Classfiy/index","parentId":"@@/global-layout"},"Home/index":{"path":"Home","id":"Home/index","parentId":"@@/global-layout"},"Own/index":{"path":"Own","id":"Own/index","parentId":"@@/global-layout"},"index":{"path":"/","id":"index","parentId":"@@/global-layout"},"@@/global-layout":{"id":"@@/global-layout","path":"/","isLayout":true}} as const;
+  const routes = {"Classfiy/index":{"path":"Classfiy","id":"Classfiy/index","parentId":"@@/global-layout"},"Home/index":{"path":"Home","id":"Home/index","parentId":"@@/global-layout"},"Note/index":{"path":"Note","id":"Note/index","parentId":"@@/global-layout"},"Own/index":{"path":"Own","id":"Own/index","parentId":"@@/global-layout"},"index":{"path":"/","id":"index","parentId":"@@/global-layout"},"@@/global-layout":{"id":"@@/global-layout","path":"/","isLayout":true}} as const;
   return {
     routes,
     routeComponents: {
 'Classfiy/index': React.lazy(() => import(/* webpackChunkName: "src__pages__Classfiy__index" */'../../../src/pages/Classfiy/index.tsx')),
 'Home/index': React.lazy(() => import(/* webpackChunkName: "src__pages__Home__index" */'../../../src/pages/Home/index.tsx')),
+'Note/index': React.lazy(() => import(/* webpackChunkName: "src__pages__Note__index" */'../../../src/pages/Note/index.tsx')),
 'Own/index': React.lazy(() => import(/* webpackChunkName: "src__pages__Own__index" */'../../../src/pages/Own/index.tsx')),
 'index': React.lazy(() => import(/* webpackChunkName: "src__pages__index" */'../../../src/pages/index.tsx')),
 '@@/global-layout': React.lazy(() => import(/* webpackChunkName: "layouts__index" */'/Users/june/Desktop/June/github/classroom-h5/src/layouts/index.tsx')),
---
+++ src/pages/Note/index.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+
+const Note:FC = ()=>{
+
+    return <div>笔记</div>
+}
+
+export default Note;
